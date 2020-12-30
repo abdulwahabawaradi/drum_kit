949db679c0c3893ac7b2deb857275bfe01d41e9a
abdulwahabawaradi <39900680+abdulwahabawaradi@users.noreply.github.com>
2020-12-30 22:26:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('play');
+}
+
+function removePlay(e){
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('play');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removePlay));
+window.addEventListener('keydown', playSound);
